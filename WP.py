5f0640bf369d8fd9a7dd0328c9541d936c110601
.Zippy <113586966+MrPlayShow@users.noreply.github.com>
2025-09-09 20:36:27 +0700

    Калькулятор через функции

---
+++ WP.py
@@ -0,0 +1,13 @@
+
+numA = (input("Введите 1 число :"))
+numB = (input("Введите 2 число :"))
+numC = (input("Введите 3 число :"))
+
+
+def sumnum (num1 = 0, num2 = 0, num3 = 0):
+    result = num1 + num2 + num3
+    
+    return result
+
+
+print((sumnum(numA,numB,numC)))
---
+++ Функции/Базовое понятие работы функции.py
@@ -0,0 +1,4 @@
+a = input("введите свое имя :")
+def hello(name):
+    print("Привет", name)
+hello(a)
